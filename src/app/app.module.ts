b5767d1d197feef741e3c5415856aba37b56a4c8
pshre1 <pshree2@sapient.com>
2018-02-02 09:29:50 +0530

    Basic routing angular 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-
+const AppRoutes: Routes = [
+  { path:'login', component:LoginComponent },
+  { path:'home', component:HomeComponent},
+  { path:'signup', component:SignupComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}, // Default redirect
+  { path:'**', component:PageNotFoundComponent}, //When there is no path match, use this for exceptions
+];
+  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    SignupComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      AppRoutes,
+      { enableTracing:true }
+    ),
     BrowserModule
   ],
   providers: [],
