1e8d15d3789cfc0db93446424201c64cd8e812ce
palavi <pshree2@sapient.com>
2018-02-06 16:34:43 +0530

    Resolve guard added for the router

---
+++ src/app/contact.resolve.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { ContactsService } from './contact.service';
+
+@Injectable()
+export class ContactResolve implements Resolve<any>{
+    constructor(private contactsService: ContactsService){
+
+    }
+
+    resolve(route:ActivatedRouteSnapshot){
+        return this.contactsService.getContact(route.params['id']);
+    }
+}
--- src/app/contactDetail.component.ts
+++ src/app/contactDetail.component.ts
@@ -4,11 +4,11 @@ import { ContactsService } from './contact.service';
 @Component({
     selector:'contact-detail',
     template:`
-    <h2>{{contact?.name}}</h2>
+    <h2>{{contact.name}}</h2>
     
     <dl>
       <dt>Twitter</dt>
-      <dd>{{contact?.twitter}}</dd>
+      <dd>{{contact.twitter}}</dd>
     </dl>
     
     <p><a routerLink="/">Back to list</a></p>
@@ -16,12 +16,13 @@ import { ContactsService } from './contact.service';
 })
 export class ContactDetailComponent implements OnInit{
     contact;
-    constructor(private contactservice: ContactsService, private route: ActivatedRoute){
-
+    // constructor(private contactservice: ContactsService, private route: ActivatedRoute){
+        constructor(private route: ActivatedRoute){
     }
 
     ngOnInit(){
-        this.contactservice.getContact(this.route.snapshot.params['id'])
-        .subscribe(contact => this.contact = contact);
+        // this.contactservice.getContact(this.route.snapshot.params['id'])
+        // .subscribe(contact => this.contact = contact);
+        this.contact = this.route.snapshot.data['contact'];
     }
 }
